6c76f694ead1e43531fdd1aace81576acc1954ef
Nywgates <aballea.louis@gmail.com>
2022-05-12 21:31:31 +0200

    Oauth 42 testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React from 'react'
+import  { Router, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
+function Loggin(){ 
+	const url = new URL("https://api.intra.42.fr/oauth/authorize");
+	url.searchParams.append('client_id', "9384000c12d958f2a599467c2bd482b25cbb0ae8e588e3b3382720ff3d290d02");
+	url.searchParams.append('redirect_uri', "http://localhost/");
+	url.searchParams.append('state', "bonjour je m'appelle pas");
+	url.searchParams.append('response_type', "code");
+	return (
+		<button onClick={event =>  window.location.href=url.toString()}>Log in 42</button>
+	)
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Loggin/>
+			</header>
+		</div>
+	);
 }
 
 export default App;
