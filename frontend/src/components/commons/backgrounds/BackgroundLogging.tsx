e5ff928ca43438d311f0b16ac7cd62f5c69f6460
Célestin Meunier <hello@celest.in>
2022-05-24 17:43:57 +0200

    finished-background

--- frontend/src/components/commons/backgrounds/BackgroundLogging.tsx
+++ frontend/src/components/commons/backgrounds/BackgroundLogging.tsx
@@ -6,8 +6,10 @@ const BackgroundLogging = () => {
 
   	return (
 		<>
-			<div className="absolute -z-1 w-screen h-screen bg-slate-900 overflow-clip">
-				<p className="font-pilowlava text-transparent text-[24vh] leading-[100%] text-center break-words backgroundTextOutline">ft_transcendence</p>
+			<div className="absolute flex justify-center items-center -z-1 w-screen h-screen bg-slate-900 overflow-clip">
+				<div className="w-[100vh]">
+					<p className="font-pilowlava text-transparent text-[24vh] leading-[100%] text-center break-words backgroundTextOutline">ft_transcendence</p>
+				</div>
 			</div>
 		</>
 	)
