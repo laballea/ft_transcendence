369527cb7f97a28d7f2c9123af47d22105e05e3f
HashYourbtc <quentin.gimenez003@gmail.com>
2022-05-28 13:41:25 +0200

    rm appcontroler

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
@@ -18,7 +16,7 @@ import { AuthModule } from './auth/auth.module';
 		UserModule,
 		AuthModule
 	],
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
