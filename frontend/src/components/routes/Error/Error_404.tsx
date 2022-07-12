badb120104082c56cf704bcf64883730aa8bc621
Celestin MEUNIER <cmeunier@e1r2p11.clusters.42paris.fr>
2022-07-12 16:56:53 +0200

    fixed 404

--- frontend/src/components/routes/Error/Error_404.tsx
+++ frontend/src/components/routes/Error/Error_404.tsx
@@ -12,23 +12,25 @@ import O from '../../../assets/svg/O.svg';
 
 function Error_404() {
   return (
-    <>
+    <div className="w-full h-full bg-slate-900">
       <NavBar/>
-      <div className='w-full h-screen text-slate-200 stroke-white'>
-        <div className='absolute flex align-center justify-center w-full h-full flex-col sm:flex-row opacity-50'>
-          <img src={number_4} alt="4" className='w-full h-[30%] sm:w-[30%] sm:h-full'/>
-          <img src={number_0} alt="4" className='w-full h-[30%] sm:w-[30%] sm:h-full'/>
-          <img src={number_4} alt="4" className='w-full h-[30%] sm:w-[30%] sm:h-full'/>
-        </div>
-        <div className='flex absolute w-full h-full '>
-          <img src={E} alt="E" className='w-[20%]'/>
-          <img src={R} alt="R" className='w-[20%]'/>
-          <img src={R} alt="R" className='w-[20%]'/>
-          <img src={O} alt="O" className='w-[20%]'/>
-          <img src={R} alt="R" className='w-[20%]'/>
+      <div className='relative top-[80px] sm:top-[112px] w-full h-[calc(100%-80px)] sm:h-[calc(100%-112px)] '>
+        <div className='absolute w-full h-full text-slate-200 stroke-white'>
+          <div className='absolute flex align-center justify-center w-[100%] h-[100%] flex-col sm:flex-row opacity-50 background-red-500'>
+            <img src={number_4} alt="4" className='w-full h-[30%] sm:w-[30%] sm:h-full'/>
+            <img src={number_0} alt="4" className='w-full h-[30%] sm:w-[30%] sm:h-full'/>
+            <img src={number_4} alt="4" className='w-full h-[30%] sm:w-[30%] sm:h-full'/>
+          </div>
+          <div className='flex absolute w-full h-full '>
+            <img src={E} alt="E" className='w-[20%]'/>
+            <img src={R} alt="R" className='w-[20%]'/>
+            <img src={R} alt="R" className='w-[20%]'/>
+            <img src={O} alt="O" className='w-[20%]'/>
+            <img src={R} alt="R" className='w-[20%]'/>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 };
 
