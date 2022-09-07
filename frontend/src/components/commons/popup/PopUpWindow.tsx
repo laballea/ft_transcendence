610cd9072ebd7366a6fe0879bf4478d5a8111be5
Celestin MEUNIER <cmeunier@e2r5p2.clusters.42paris.fr>
2022-09-07 17:26:04 +0200

    edited toaster

--- frontend/src/components/commons/popup/PopUpWindow.tsx
+++ frontend/src/components/commons/popup/PopUpWindow.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FiAlertOctagon, FiSmile } from 'react-icons/fi'
 
 type PopUpWindowProps = {
 	content : string,
@@ -8,21 +9,25 @@ type PopUpWindowProps = {
 const PopUpWindow = ({ content, error } : PopUpWindowProps) => {
 
 	return (
-		<div className=" absolute bottom-0 left-0 p-[12px] w-fit ">
-				<p	
-					className="rounded
-								flex justify-left items-center
-								w-[300px] p-[24px] h-[48px]
-								bg-slate-700
-								font-space text-[16px] text-slate-500 hover:text-slate-400
-								transition-all duration-500 ease-in-out
-								shadow-addFriend"
-					style={{color:error ? "#902923" : "#2CDA9D"}}
-				>
-					{content}
-				</p>
-		</div>
-	)
+		<>
+				<div className={	error === true ? 
+									"bg-red-500/50 text-red-500 p-[16px]	 absolute flex items-center gap-[16px] bottom-[24px] left-[24px] font-space text-[16px] whitespace-nowrap transition-all duration-500 ease-in-out " 
+									: 
+									"bg-green-500/50 text-green-500 p-[16px] absolute flex items-center gap-[16px] bottom-[24px] left-[24px] font-space text-[16px] whitespace-nowrap transition-all duration-500 ease-in-out "
+								}
+				>		
+							{ error === true ? 
+							<FiAlertOctagon className='text-[24px]'/>
+							:
+							<FiSmile className='text-[24px]'/>
+						}
+
+						<p>
+							{content}
+						</p>
+				</div>
+		</>
+	) 
 }
 
 PopUpWindow.defaultProps = {
