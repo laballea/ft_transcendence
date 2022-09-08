fcba2475ce665f89548f35e058e5b8009a870bc8
Celestin MEUNIER <cmeunier@e1r10p11.clusters.42paris.fr>
2022-09-08 18:02:11 +0200

    first commit

--- frontend/src/components/routes/root/Logging.tsx
+++ frontend/src/components/routes/root/Logging.tsx
@@ -10,10 +10,31 @@ import { useNavigate } from 'react-router-dom';
 import Popup from 'reactjs-popup'; 
 import { useDispatch, useSelector } from 'react-redux';
 import PopUpToaster from '../../commons/popup/PopUpToaster';
+import { stringify } from 'querystring';
 
 const Logging = () => {
 	const [username, setUsername] = useState("");
-	const [code2fa, setCode] = useState("");
+
+	type pinCode = {
+		pin1 : string,
+		pin2 : string,
+		pin3 : string,
+		pin4 : string,
+		pin5 : string,
+		pin6 : string,
+
+	}
+	const code2faInitialState = {
+		pin1: "",
+		pin2: "",
+		pin3: "",
+		pin4: "",
+		pin5: "",
+		pin6: "",
+	}
+
+	const [code2fa, setCode] = useState(code2faInitialState);
+
 	const global = useSelector((state: any) => state.global)
 	const dispatch = useDispatch();
 	const [popup, setPopup] = useState({open:false, error:true, message:""});
@@ -58,7 +79,7 @@ const Logging = () => {
 			}
 		})
 	}
-	
+
 	/*
 		POST username at auth/login, back respond with jwt token
 		and we redirect to home with jwt in query
@@ -87,16 +108,19 @@ const Logging = () => {
 	}
 
 	const handleCode = async (event: any) => {
+
+		const concatCode :string = code2fa.pin1 + code2fa.pin2 + code2fa.pin3 + code2fa.pin4 + code2fa.pin5 + event.target.value
+		
 		event.preventDefault();// Prevent page reload
 		const requestOptions = {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json;charset=utf-8',
 				'Access-Control-Allow-Origin': '*',
-				//'Authorization': 'bearer ' + jwt,
+				//'Authorization': 'bearer ' + jwt,w-[64px] h-[80px]
 			},
 			body: JSON.stringify({
-				code: code2fa,
+				code: concatCode,
 				id:id
 			})
 		}
@@ -106,27 +130,105 @@ const Logging = () => {
 			if (response.ok){
 				validToken(resp.token)
 			} else {
-				setPopup({open:true, error:true, message:resp.message})
+				setCode(code2faInitialState)
+				// setPopup({open:true, error:true, message:resp.message})
+	
 			}
 		})
 	}
+	
+	for (const [key, value] of Object.entries(code2fa)) {
+		
+		if (value === "")
+		{
+			let nextSibiling = document.getElementById(`${key}`);
+
+			if(nextSibiling !== null){
+				nextSibiling.focus();
+				break;
+			}
+		}
+	}
+
 
 	if (id && twofa) {
 		return (
+		<div className='w-full h-full flex items-center justify-center '>
 			<div>
-				<form onSubmit={handleCode}>
-					<input className="h-8 w-[260px] p-4 mb-2 font-space bg-transparent border-2 border-slate-400 hover:border-slate-200
-										text-md text-slate-400 placeholder:text-slate-600 rounded transition-all duration-300 ease-in-out"
-						type="text"
-						placeholder="code 2fa"
-						value={code2fa}
-						onChange={e => setCode(e.target.value)} required
-					/>
-					<button className="h-8 font-space text-slate-400 bg-slate-600" type="submit">
+				
+				<form className="relative z-10 w-[472px]">
+					<label className='absolute top-[-32px] font-space text-slate-500'>Enter your 2FA code here: </label>
+					<div className='flex items-center justify-center gap-[16px]'>
+						<input className='w-[64px] h-[80px] border-2 border-slate-700
+											bg-transparent text-center
+											font-space text-[72px] text-slate-300' 
+								onChange={e => {
+									setCode({...code2fa, pin1 : e.target.value})
+									
+								}}
+								value={code2fa.pin1}
+								tabIndex={1}
+								type="tel" id="pin1" maxLength={1} pattern="[\d]*" placeholder="·" autoComplete="off"  required></input>
+						<input className='w-[64px] h-[80px] border-2 border-slate-700
+											bg-transparent text-center
+											font-space text-[72px] text-slate-300' 
+								onChange={e => {
+									setCode({...code2fa, pin2 : e.target.value})
+									
+								}}
+								value={code2fa.pin2}
+								tabIndex={2}
+								type="tel" id="pin2" maxLength={1} pattern="[\d]*" placeholder="·" autoComplete="off"  required></input>
+						<input className='w-[64px] h-[80px] border-2 border-slate-700
+											bg-transparent text-center
+											font-space text-[72px] text-slate-300' 
+								onChange={e => {
+									setCode({...code2fa, pin3 : e.target.value})
+									
+								}}
+								value={code2fa.pin3}
+								tabIndex={3}
+								type="tel" id="pin3" maxLength={1} pattern="[\d]*" placeholder="·" autoComplete="off"  required></input>
+						<input className='w-[64px] h-[80px] border-2 border-slate-700
+											bg-transparent text-center
+											font-space text-[72px] text-slate-300' 
+								onChange={e => {
+									setCode({...code2fa, pin4 : e.target.value})
+									
+								}}
+								value={code2fa.pin4}
+								tabIndex={4}
+								type="tel" id="pin4" maxLength={1} pattern="[\d]*" placeholder="·" autoComplete="off"  required></input>
+						<input className='w-[64px] h-[80px] border-2 border-slate-700
+											bg-transparent text-center
+											font-space text-[72px] text-slate-300' 
+								onChange={e => {
+									setCode({...code2fa, pin5 : e.target.value})
+									
+								}}
+								value={code2fa.pin5}
+								tabIndex={5}
+								type="tel" id="pin5" maxLength={1} pattern="[\d]*" placeholder="·" autoComplete="off"  required></input>
+						<input className='w-[64px] h-[80px] border-2 border-slate-700
+											bg-transparent text-center
+											font-space text-[72px] text-slate-300' 
+								onChange={e => {
+									handleCode(e)
+								}}
+								value={code2fa.pin6}
+								tabIndex={6}
+								type="tel" id="pin6" maxLength={1} pattern="[\d]*" placeholder="·" autoComplete="off"  required></input>
+					</div>
+					<input type="submit" hidden />
+					{/* <button className="text-white" type="submit">
 						Send
-					</button>
+					</button> */}
 				</form>
 			</div>
+			<Popup open={popup.open} contentStyle={{position:'absolute', bottom:0, left:0}}>
+				<PopUpToaster content={popup.message}/>
+			</Popup>
+		</div>
 		)
 	} else if (jwt){ // if token exist in redux user is already logged
 		const requestOptions = {
