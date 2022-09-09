1b76792d8015b2bac0c9216db6966c4faf1892af
Celestin MEUNIER <cmeunier@e2r9p6.clusters.42paris.fr>
2022-09-10 00:59:05 +0200

    first commit

--- frontend/src/components/message/CreateRoom.tsx
+++ frontend/src/components/message/CreateRoom.tsx
@@ -1,11 +1,12 @@
 import React, { useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import IconButton from '../commons/buttons/IconButton';
-import { FiX } from 'react-icons/fi';
+import { FiArrowUpCircle, FiKey, FiPlusSquare, FiX } from 'react-icons/fi';
 import { setCreateRoom } from '../../store/global/reducer';
 import './noScrollBar.css'
 import { joinRoomSocket, newChatRoom } from '../../context/socket';
 import ChatBar from './chatBar';
+import MiniIconButton from '../commons/buttons/MiniIconButton';
 
 function CreateRoom() {
 	const global = useSelector((state: any) => state.global)
@@ -25,7 +26,8 @@ function CreateRoom() {
 		})
 	}
 
-	const pushRoom = ():void => {
+	const pushRoom = (e : any):void => {
+		e.preventDefault()
 		if (input.password !== input.confirmed)
 			setErrorPass(true)
 			//alert("password does not match !")
@@ -43,7 +45,8 @@ function CreateRoom() {
 		})
 	}
 
-	const joinRoom = ():void => {
+	const joinRoom = (e : any):void => {
+		e.preventDefault()
 		joinRoomSocket(global, input.roomName, input.password)
 		setInput({
 			roomName: "",
@@ -66,110 +69,180 @@ function CreateRoom() {
 						rounded-md
 						drop-shadow-custom1
 						'>
-			<div className='w-full h-auto p-[8px] flex items-center justify-between bg-slate-700 drop-shadow-custom2'>
-				<h3 className={`font-space cursor-pointer ${mode === 1 ? "text-green-200" : "text-slate-200"}`} onClick={()=>{setMode(mode === 1 ? 0 : 1)}}>
-					create room
-				</h3>
-				<h3 className={`font-space cursor-pointer ${mode === 2 ? "text-green-200" : "text-slate-200"}`} onClick={()=>{setMode(mode === 2 ? 0 : 2)}}>
-					join room
-				</h3>
-				<IconButton icon={FiX} onClick={()=>{dispatch(setCreateRoom({}))}}></IconButton>
+			<div className='w-full h-auto p-4 flex items-center justify-between
+							drop-shadow-custom2'>
+				<p className='font-pilowlava text-slate-600
+								hover:text-slate-200 transition-all duration-300 ease-in-out
+								cursor-pointer' 
+								onClick={()=>{setMode(0)}}>Chats</p>
+				<div className='h-[24px] flex gap-2'>
+					<div className={`${mode === 1 ? 'text-purple-500':'text-slate-400'} 
+									text-[24px]
+									hover:text-slate-200 transition-all duration-300 ease-in-out
+									cursor-pointer`}>
+						<FiPlusSquare onClick={()=>{setMode(mode === 1 ? 0 : 1)}}/>
+					</div>
+					<div className={`${mode === 2 ? 'text-purple-500':'text-slate-400'} 
+									text-[24px]
+									hover:text-slate-200 transition-all duration-300 ease-in-out
+									cursor-pointer`}>
+						<FiKey onClick={()=>{setMode(mode === 2 ? 0 : 2)}}/>
+					</div>
+					
+				</div>
 			</div>
+			<div className='h-[1px] bg-slate-800 mr-4 ml-4'></div>
 			{mode === 0 && 
 				<ChatBar/>
 			}
 			{mode === 1 &&
-				<div  className='flex flex-grow flex-col items-center'>
-					<input
-						className={`flex h-[50px]
-									p-[8px] pl-[12px] rounded-sm
-									bg-slate-800 text-slate-200 placeholder:text-slate-400
-									font-space text-[16px]
-									m-5 ${errorName && "border-2 border-red-400"}
-									`}
-						type="text"
-						placeholder="room name"
-						value={input.roomName}
-						onChange={handleChange}
-						name="roomName"
-					/>
-					<input
-						className="	flex h-[50px]
-									p-[8px] pl-[12px] rounded-sm
-									bg-slate-800 text-slate-200 placeholder:text-slate-400
-									font-space text-[16px]
-									m-5
-									"
-						type="password"
-						placeholder="password"
-						value={input.password}
-						onChange={handleChange}
-						name="password"
-					/>
-					<input
-						className={`flex h-[50px]
-									p-[8px] pl-[12px] rounded-sm
-									bg-slate-800 text-slate-200 placeholder:text-slate-400
-									font-space text-[16px]
-									m-5 ${errorPass && "border-2 border-red-400"}
-									`}
-						type="password"
-						placeholder="confirm password"
-						value={input.confirmed}
-						onChange={handleChange}
-						name="confirmed"
-					/>
+				<form
+					onSubmit={pushRoom} 
+					className='flex flex-grow w-full flex-col items-center justify-between p-4'>
+					<div className='flex flex-col gap-2 w-full'>
+						<input
+							className="flex 
+										h-12 w-full 
+										p-[8px] pl-[12px] rounded-sm
+										bg-slate-800 text-slate-200 placeholder:text-slate-400
+										font-space text-[16px]
+										"
+							type="text"
+							placeholder="room name"
+							value={input.roomName}
+							onChange={handleChange}
+							name="roomName"
+							maxLength={15}
+							autoComplete="off"
+							required
+						/>
+						<input
+							className="	flex h-12 w-full
+										p-[8px] pl-[12px] rounded-sm
+										bg-slate-800 text-slate-200 placeholder:text-slate-400
+										font-space text-[16px]
+						
+										"
+							type="password"
+							placeholder="password"
+							value={input.password}
+							onChange={handleChange}
+							autoComplete="off"
+							name="password"
+						/>
+						<input
+							className={`flex h-12 w-full
+										p-[8px] pl-[12px] rounded-sm
+										bg-slate-800 text-slate-200 placeholder:text-slate-400
+										font-space text-[16px]
+										${errorPass && "border-2 border-red-400"}
+										`}
+							type="password"
+							placeholder="confirm password"
+							value={input.confirmed}
+							onChange={handleChange}
+							name="confirmed"
+							autoComplete="off"
+						/>
+					</div>
 					<button	
-						className={`bg-transparent border-2 h-[32px] sm:h-[48px] w-[160px] sm:w-[164px] rounded
+						type='submit'
+						className={`bg-transparent border-2 h-[32px] sm:h-[48px] w-full rounded
 									font-space text-[16px] text-slate-400
 									transition-all duration-300 ease-in-out
-									flex justify-center items-center ${errorName || errorPass ? "border-red-400" : "border-green-400 hover:border-green-200 hover:text-green-200 text-green-400"}`}
-						onClick={pushRoom}
-						disabled={errorName || errorPass}
+									flex justify-center items-center ${errorPass ? "border-red-400" : "border-slate-400 hover:border-slate-200 hover:text-slate-200 text-slate-400"}`}
+						
 						>
 						<p>Create</p>
 					</button>
-				</div>
+				</form>
 				}
 				{mode === 2 &&
-				<div  className='flex flex-grow flex-col items-center'>
+				<form
+				onSubmit={joinRoom} 
+				className='flex flex-grow w-full flex-col items-center justify-between p-4'>
+				<div className='flex flex-col gap-2 w-full'>
 					<input
-						className={`flex h-[50px]
+						className="flex 
+									h-12 w-full 
 									p-[8px] pl-[12px] rounded-sm
 									bg-slate-800 text-slate-200 placeholder:text-slate-400
 									font-space text-[16px]
-									m-5 ${errorName && "border-2 border-red-400"}
-									`}
+									"
 						type="text"
 						placeholder="room name"
 						value={input.roomName}
 						onChange={handleChange}
 						name="roomName"
+						maxLength={15}
+						autoComplete="off"
+						required
 					/>
 					<input
-						className="	flex h-[50px]
+						className="	flex h-12 w-full
 									p-[8px] pl-[12px] rounded-sm
 									bg-slate-800 text-slate-200 placeholder:text-slate-400
 									font-space text-[16px]
-									m-5
+					
 									"
 						type="password"
 						placeholder="password"
 						value={input.password}
 						onChange={handleChange}
+						autoComplete="off"
 						name="password"
 					/>
-					<button	
-						className={`bg-transparent border-2 h-[32px] sm:h-[48px] w-[160px] sm:w-[164px] rounded
-									font-space text-[16px] text-slate-400
-									transition-all duration-300 ease-in-out
-									flex justify-center items-center ${errorName ? "border-red-400" : "border-green-400 hover:border-green-200 hover:text-green-200 text-green-400"}`}
-						onClick={joinRoom}
-						disabled={errorName}
-						>
-						<p>Join</p>
-					</button>
 				</div>
+				<button	
+					type='submit'
+					className="bg-transparent border-2 h-[32px] sm:h-[48px] w-full rounded
+								font-space text-[16px] text-slate-400
+								transition-all duration-300 ease-in-out
+								flex justify-center items-center"
+					
+					>
+					<p>Join</p>
+				</button>
+			</form>
+				// <form  className='flex flex-grow flex-col items-center'>
+				// 	<input
+				// 		className={`flex h-12
+				// 					p-[8px] pl-[12px] rounded-sm
+				// 					bg-slate-800 text-slate-200 placeholder:text-slate-400
+				// 					font-space text-[16px]
+				// 					m-5 ${errorName && "border-2 border-red-400"}
+				// 					`}
+				// 		type="text"
+				// 		placeholder="room name"
+				// 		value={input.roomName}
+				// 		onChange={handleChange}
+				// 		name="roomName"
+				// 	/>
+				// 	<input
+				// 		className="	flex h-12
+				// 					p-[8px] pl-[12px] rounded-sm
+				// 					bg-slate-800 text-slate-200 placeholder:text-slate-400
+				// 					font-space text-[16px]
+				// 					m-5
+				// 					"
+				// 		type="password"
+				// 		placeholder="password"
+				// 		value={input.password}
+				// 		onChange={handleChange}
+				// 		name="password"
+				// 	/>
+				// 	<button	
+					
+				// 		className={`bg-transparent border-2 h-[32px] sm:h-[48px] w-[160px] sm:w-[164px] rounded
+				// 					font-space text-[16px] text-slate-400
+				// 					transition-all duration-300 ease-in-out
+				// 					flex justify-center items-center ${errorName ? "border-red-400" : "border-green-400 hover:border-green-200 hover:text-green-200 text-green-400"}`}
+				// 		onClick={joinRoom}
+				// 		disabled={errorName}
+				// 		>
+				// 		<p>Join</p>
+				// 	</button>
+				// </form>
 			}
 			
 		</div>
