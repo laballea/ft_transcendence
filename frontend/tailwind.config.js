2a74d08d110852cc60491e6bce46bd2e4b35902a
Celestin MEUNIER <cmeunier@e1r10p11.clusters.42paris.fr>
2022-09-12 14:44:37 +0200

    first commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       boxShadow: {
         'addFriend': '0px 0px 12px 8px #1E293B',
+        'innerXL' : 'inset 0px 4px 24px 0px rgba(0,0,0,0.4)'
       },
       animation: {
         'vibrate' : 'vibrate 0.3s linear infinite both',
